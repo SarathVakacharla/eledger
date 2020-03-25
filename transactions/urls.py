ed1081198eadce5165bbda0d9a383dff22b9d00a
Sarath Vakacharla <sarath.vakacharla@7-11.com>
2020-03-25 18:02:52 -0500

    seperated farmers and companies

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,12 +3,17 @@ from . import views
 from django.conf.urls.static import static
 urlpatterns = [
 	path('', views.index, name = 'index'),
-	path('farmcomp/', views.actors, name = 'actors'),
 	#path('txn/list/', views.TransactionList.as_view(), name='transaction_list'),
 	path('txn/list/', views.index, name = 'transaction_list'),
 	path('txn/new/', views.TransactionCreate.as_view(), name='transaction_new'),
 	path('txn/view/<int:pk>', views.TransactionView.as_view(), name='transaction_view'),
 	path('txn/edit/<int:pk>', views.TransactionUpdate.as_view(), name='transaction_edit'),
-	#path('Create-Farmer/', views.create_farmer, name = 'create-farmer'),
-	#path('Create-Company/', views.create_company, name = 'create-company')
+
+	path('farmer/list/', views.farmers, name = 'farmer_list'),
+	path('farmer/new/', views.FarmerCreate.as_view(), name='farmer_new'),
+	path('farmer/edit/<int:pk>', views.FarmerUpdate.as_view(), name='farmer_edit'),
+
+	path('company/list/', views.companies, name = 'company_list'),
+	path('company/new/', views.CompanyCreate.as_view(), name='company_new'),
+	path('company/edit/<int:pk>', views.CompanyUpdate.as_view(), name='company_edit'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -7,61 +7,9 @@ from django.shortcuts import render, redirect
 from .models import Farmer, Company, Transaction
 from .forms import CreateFarmer, CreateCompany, CreateTransaction
 
-def get_all_actors():
-	farmers = Farmer.objects.all()
-	companies = Company.objects.all()
-	actors = {
-		'farmers' : farmers,
-		'companies' : companies
-	}
-	return actors
-
+## Transactions ##
 def index(request):
 	return render(request, 'transactions/transaction_list.html', {'transactions': Transaction.objects.all()})
-
-def actors(request):
-	return render(request, 'farmers_companies.html', {'actors': get_all_actors()})
-
-def create_farmer(request):
-	if request.method == 'POST':
-		new_farmer = CreateFarmer(request.POST)
-		if new_farmer.is_valid():
-			new_farmer.save()
-			return redirect('actors')
-		else:
-			return HttpResponse("""Unable to create farmer, try again <a href = "{{ url : 'actors'}}">reload</a>""") #TODO: give link to create page
-	else:
-		context = {
-			'actors': get_all_actors(),
-			'status' : {
-				'disposition' : "FAILURE",
-				'message' : "Unable to created farmer" #TODO: add farmer name
-			}
-		}
-		return render(request, 'farmers_companies.html', context)
-
-# TODO: update farmers
-
-
-def create_company(request):
-	if request.method == 'POST':
-		new_company = CreateCompany(request.POST)
-		if new_company.is_valid():
-			new_company.save()
-			return redirect('actors')
-		else:
-			return HttpResponse("""Unable to create company, try again <a href = "{{ url : 'actors'}}">reload</a>""") #TODO: give link to create page
-	else:
-		context = {
-			'actors': get_all_actors(),
-			'status' : {
-				'disposition' : "SUCCESS",
-				'message' : "Succesfully created company" #TODO: add ompany name
-			}
-		}
-		return render(request, 'farmers_companies.html', context)
-
-# TODO: update companies
 
 class TransactionList(ListView):
 	model = Transaction
@@ -78,3 +26,32 @@ class TransactionUpdate(UpdateView):
 	model = Transaction
 	fields = '__all__'
 	success_url = reverse_lazy('transaction_list')
+
+def farmers(request):
+	return render(request, 'transactions/farmer_list.html', {'farmers': Farmer.objects.all()})
+
+## Farmers ##
+class FarmerCreate(CreateView):
+	model = Farmer
+	fields = '__all__'
+	success_url = reverse_lazy('farmer_list')
+
+class FarmerUpdate(UpdateView):
+	model = Farmer
+	fields = '__all__'
+	success_url = reverse_lazy('farmer_list')
+
+def companies(request):
+	return render(request, 'transactions/company_list.html', {'companies': Company.objects.all()})
+	
+## Companies ##
+class CompanyCreate(CreateView):
+	model = Company
+	fields = '__all__'
+	success_url = reverse_lazy('company_list')
+
+class CompanyUpdate(UpdateView):
+	model = Company
+	fields = '__all__'
+	success_url = reverse_lazy('company_list')
+
